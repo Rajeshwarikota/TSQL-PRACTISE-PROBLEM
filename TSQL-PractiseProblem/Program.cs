0199797088341c8fce4d24a8f1a0c4e082ae3aef
“Rajeshwari <kotarajeswari003@gmail.com>
2023-04-23 23:34:02 +0530

    [Rajeshwari kota] Adding TSQL Practise Problem

---
+++ TSQL-PractiseProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSQL_PractiseProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....TSQL-PRACTISE-PROBLEM....");
+            Console.ReadLine();
+        }
+    }
+}
